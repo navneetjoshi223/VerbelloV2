6469ee130221ec7f1473e546dd6b9b5e5ee506c8
navneetjoshi223 <joshi.nav@northeastern.edu>
2023-12-03 20:15:15 -0500

    Initial Home page and initial Navbar + temp logo

--- verbello/frontend/src/App.js
+++ verbello/frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          {/* <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/lesson" element={<Lesson />} />
+          <Route path="/profile" element={<Profile />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
