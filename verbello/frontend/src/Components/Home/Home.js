8480dd6ed9c758fbdb6fb533e1aa516c80047ff3
navneetjoshi223 <joshi.nav@northeastern.edu>
2023-12-05 21:08:49 -0500

    added flags in "public", added Languages in homepage

--- verbello/frontend/src/Components/Home/Home.js
+++ verbello/frontend/src/Components/Home/Home.js
@@ -1,13 +1,42 @@
 import React from "react";
 import Navbar from "../common/Navbar/Navbar";
 import IntroCard from "../IntroCard/IntroCard";
+import "./Home.css";
 //import Testimonials from "../Testimonials/Testimonials";
+//import franceFlag from '../../assets/images/french/france.jpeg';
 
 const Home = () => {
+  const offerings = [
+    { language: "French", img: "./images/flags/france.jpeg" },
+    { language: "German", img: "./images/flags/germany.jpeg" },
+    { language: "Spanish", img: "./images/flags/spain.jpeg" },
+    { language: "Portuguese", img: "./images/flags/portugal.jpeg" },
+    { language: "Italian", img: "./images/flags/italy.jpeg" },
+  ];
+
   return (
     <>
       <Navbar />
       <IntroCard />
+      <div class="card languages-offered m-3 p-3">
+        <div class="card-body">
+          <h3 class="card-title custom-header pb-4">Languages we offer</h3>
+          <div className="card-group">
+            {offerings.map((offering, index) => (
+              <div className="card card-language border-0 p-3" key={index}>
+                <img
+                  src={offering.img}
+                  className="card-img-top country"
+                  alt={offering.language}
+                />
+                <div className="card-body">
+                  <h5 className="card-title text-center language-name">{offering.language}</h5>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- verbello/frontend/src/Components/common/Navbar/Navbar.js
+++ verbello/frontend/src/Components/common/Navbar/Navbar.js
@@ -29,7 +29,7 @@ const Navbar = () => {
           </ul>
 
           <Link to="/login" className="login">
-            <button type="button" className="btn btn-primary mx-1">
+            <button type="button" className="btn btn-outline-light mx-1">
               Login
             </button>
           </Link>
