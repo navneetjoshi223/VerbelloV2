0a1c748f202aa0a446a4523e944e0d19c3c118fa
VChoukwale <choukwale.v@northeastern.edu>
2023-12-06 00:58:56 -0800

    Added quiz component and questionnaire

--- verbello/frontend/src/App.js
+++ verbello/frontend/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
+import QuizQuestions from './Components/Quiz/QuizQuestions';
+import questions from './questions';
 
 const App = () => {
   return (
@@ -9,6 +11,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/quiz" element={<QuizQuestions questions={questions} />} />
           {/* <Route path="/login" element={<Login/>} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/aboutus" element={<AboutUs />} />
---
+++ verbello/frontend/src/questions.js
@@ -0,0 +1,46 @@
+const questions = [
+    {
+      question: "What is the capital of France?",
+      options: ["Berlin", "Madrid", "Paris", "Rome"],
+      correctAnswer: 2, 
+    },
+    {
+      question: "Which river runs through Paris?",
+      options: ["Seine", "Thames", "Danube", "Rhine"],
+      correctAnswer: 0,
+    },
+    {
+        id: 1,
+        question: 'What is a dog called in French?',
+        options: ['Chat', 'Chien', 'Cheval', 'Poisson'],
+        correctAnswer: 'Chien',
+      },
+      {
+        id: 2,
+        question: 'How do you say "cat" in French?',
+        options: ['Chien', 'Chat', 'Poulet', 'Souris'],
+        correctAnswer: 'Chat',
+      },
+      {
+        id: 3,
+        question: 'What is the French name for a fish?',
+        options: ['Oiseau', 'Poisson', 'Lion', 'Grenouille'],
+        correctAnswer: 'Poisson',
+      },
+      {
+        id: 4,
+        question: 'Translate "horse" into French.',
+        options: ['Cheval', 'Vache', 'Tigre', 'Serpent'],
+        correctAnswer: 'Cheval',
+      },
+      {
+        id: 5,
+        question: 'How do you say "bird" in French?',
+        options: ['Chien', 'Souris', 'Oiseau', 'Éléphant'],
+        correctAnswer: 'Oiseau',
+      },
+    
+  ];
+  
+  export default questions;
+  
