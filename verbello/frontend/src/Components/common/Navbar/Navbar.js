960ecff2325f4d077471abd3a7d8d744d8edee72
VChoukwale <choukwale.v@northeastern.edu>
2023-12-06 19:34:37 -0800

    Added cusom properties for hamburger menu

--- verbello/frontend/src/Components/common/Navbar/Navbar.js
+++ verbello/frontend/src/Components/common/Navbar/Navbar.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 //import verbelloLogo from "../../../assets/images/verbello-logo.jpeg";
 
 const Navbar = () => {
+
+  const [togglerColor, setTogglerColor] = useState("#ffffff");
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleToggleClick = () => {
+    // Toggle the color between white and green
+    const newColor = togglerColor === "#ffffff" ? "#00ff00" : "#ffffff";
+    setTogglerColor(newColor);
+    setIsNavCollapsed(!isNavCollapsed);
+  };
+
   return (
-    <nav className="navbar sticky-top navbar-expand-lg navbar-bgcolor p-3">
+    <nav className="navbar sticky-top navbar-expand-md navbar-bgcolor p-3">
       <div className="container-fluid">
         <Link to="/" className="navbar-logo">
           <span className="logo-txt">Verbello</span>
@@ -13,23 +24,28 @@ const Navbar = () => {
         </Link>
 
         <button
-          className="navbar-toggler"
+          className={`navbar-toggler custom-toggler ${
+            isNavCollapsed ? "" : "collapsed"
+          }`} 
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNavDropdown"
           aria-controls="navbarNavDropdown"
-          aria-expanded="false"
+          aria-expanded={!isNavCollapsed}  
           aria-label="Toggle navigation"
+          style={{ backgroundColor: togglerColor, borderColor: "#333" }}
+          onClick={handleToggleClick}
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-
         <div
-          className="collapse navbar-collapse justify-content-end"
+          className={`collapse navbar-collapse justify-content-end ${
+            isNavCollapsed ? "" : "show"
+          }`}
           id="navbarNavDropdown"
         >
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav ms-auto mb-lg-0">
             <li className="nav-item">
               <Link to="/contactus" className="nav-link text-white">
                 Contact Us
@@ -43,13 +59,13 @@ const Navbar = () => {
           </ul>
 
           <Link to="/login" className="login">
-            <button type="button" className="btn btn-outline-light mx-1">
+            <button type="button" className="btn btn-outline-light mx-1 mt-0">
               Login
             </button>
           </Link>
 
           <Link to="/signup" className="signup">
-            <button type="button" className="btn btn-light mx-1">
+            <button type="button" className="btn btn-light mx-1 mt-0">
               Sign Up
             </button>
           </Link>
